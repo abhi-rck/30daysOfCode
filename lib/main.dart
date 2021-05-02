ba93c6cb61722c749fedb62c08322b0bc2978c81
Abhinav Gupta <2018uee1524@mnit.ac.in>
2021-05-02 22:11:41 +0530

    day2

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:shop/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  int days = 30;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
