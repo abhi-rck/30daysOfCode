ad6aeb26518aa59c331b0916cae5e741e83e95cc
Abhinav Gupta <2018uee1524@mnit.ac.in>
2021-05-03 10:38:12 +0530

    day

---
+++ lib/models/product.dart
@@ -0,0 +1,21 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final String color;
+  final String image;
+  final num price;
+
+  Item({this.id, this.name, this.desc, this.color, this.image, this.price});
+}
+
+final products = [
+  Item(
+      color: "#335058",
+      id: "product1",
+      name: "jeans X",
+      desc: "MultiBuild functionality jeans",
+      image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT8BJp76DjX0mgNvIh7PLf1edgt_gZmK6yvF2t3lP7TmFYfLbaccbx3_jBotFzQcyQJAdqFog&usqp=CAc",
+      price: 10.5)
+];
